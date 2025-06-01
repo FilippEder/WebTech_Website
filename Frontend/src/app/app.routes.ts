701dab8aef2d4a73dfdccc9bcc6ee21e15708394
filipped <82341252+FilippEder@users.noreply.github.com>
2025-06-01 13:57:49 +0200

    Separate Dashboard from app.ts

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -38,29 +38,44 @@ import {ChangeEmailComponent} from './components/login-register-components/chang
 import { ListProductComponent } from './components/retail/owner/list-product/list-product.component';
 import {ProductSearchBrowserComponent} from './components/retail/buyer/product-search-browser/product-search-browser.component';
 import { ProductDetailComponent } from './components/retail/buyer/product-detail/product-detail.component';
+import {Dashboard} from './components/dashboard/dashboard';
 
 export const routes: Routes = [
-  {path: 'vehicle-seller', component: VehicleSellerComponent, canActivate:[authGuard]},
-  {path: 'update-vehicle/:id', component: UpdateVehicleComponent, canActivate:[authGuard]},
-  {path: 'vehicle-buyer', component:VehicleBuyerComponent, canActivate:[authGuard]},
-  {path: 'vehicle-description/:id', component:VehicleDescriptionViewComponent, canActivate:[authGuard]},
-  {path: 'register',component:RegisterComponent, canActivate:[loginGuard]},
-  {path: 'login',component:LoginComponent, canActivate:[loginGuard]},
-  {path: 'change-password',component:ChangePasswordComponent, canActivate:[authGuard]},
-  {path: 'change-email', component:ChangeEmailComponent, canActivate:[authGuard]},
-  { path: 'chat/:requestId', component: ChatPageComponent, canActivate:[authGuard] },
-  { path: 'owner/listings', component: ListingOverviewComponent, canActivate:[authGuard] },
-  { path: 'owner/add', component: AddRealEstateComponent , canActivate:[authGuard] },
-  { path: 'owner/edit/:id', component: EditRealEstateComponent , canActivate:[authGuard]},
-  { path: 'tenant/requests', component: RequestUebersichtComponent , canActivate:[authGuard]},
-  { path: 'owner/requests', component: RequestOverviewComponentOwner , canActivate:[authGuard]},
-  { path: 'tenant/search', component: SearchBrowserComponent , canActivate:[authGuard]},
-  { path: 'tenant/details/:id', component: DetailedViewComponent , canActivate:[authGuard]},
-  { path: 'chat/:requestId', component: ChatPageComponent , canActivate:[authGuard]},
-  {path: 'real-estate-add', component: AddRealEstateComponent , canActivate:[authGuard]},
-  {path: 'real-estate-edit', component: EditRealEstateComponent , canActivate:[authGuard]},
-  {path: 'real-estate-browser', component: SearchBrowserComponent , canActivate:[authGuard]},
-  {path: 'retail-add', component: ListProductComponent, canActivate:[authGuard]},
-  {path: 'retail-browser', component: ProductSearchBrowserComponent, canActivate:[authGuard]},
-  {path: 'product-detail/:id', component: ProductDetailComponent, canActivate: [authGuard]}
+  {path: '',
+  component: Dashboard,
+  children: [
+    //authentication
+    {path: '', component:LoginComponent, canActivate:[loginGuard]},
+    {path: 'login',component:LoginComponent, canActivate:[loginGuard]},
+    {path: 'register',component:RegisterComponent, canActivate:[loginGuard]},
+    {path: 'change-password',component:ChangePasswordComponent, canActivate:[authGuard]},
+    {path: 'change-email', component:ChangeEmailComponent, canActivate:[authGuard]},
+
+    //vehicles
+    {path: 'vehicle-buyer', component:VehicleBuyerComponent, canActivate:[authGuard]},
+    {path: 'vehicle-seller', component: VehicleSellerComponent, canActivate:[authGuard]},
+    {path: 'update-vehicle/:id', component: UpdateVehicleComponent, canActivate:[authGuard]},
+    {path: 'vehicle-description/:id', component:VehicleDescriptionViewComponent, canActivate:[authGuard]},
+
+    //real-estate
+    {path: 'real-estate-add', component: AddRealEstateComponent , canActivate:[authGuard]},
+    {path: 'real-estate-edit', component: EditRealEstateComponent , canActivate:[authGuard]},
+    {path: 'real-estate-browser', component: SearchBrowserComponent , canActivate:[authGuard]},
+    { path: 'owner/listings', component: ListingOverviewComponent, canActivate:[authGuard] },
+    { path: 'owner/add', component: AddRealEstateComponent , canActivate:[authGuard] },
+    { path: 'owner/edit/:id', component: EditRealEstateComponent , canActivate:[authGuard]},
+    { path: 'owner/requests', component: RequestOverviewComponentOwner , canActivate:[authGuard]},
+    { path: 'tenant/requests', component: RequestUebersichtComponent , canActivate:[authGuard]},
+    { path: 'tenant/search', component: SearchBrowserComponent , canActivate:[authGuard]},
+    { path: 'tenant/details/:id', component: DetailedViewComponent , canActivate:[authGuard]},
+
+    //retail
+    {path: 'retail-add', component: ListProductComponent, canActivate:[authGuard]},
+    {path: 'retail-browser', component: ProductSearchBrowserComponent, canActivate:[authGuard]},
+    {path: 'product-detail/:id', component: ProductDetailComponent, canActivate: [authGuard]},
+
+    //chat
+    { path: 'chat/:requestId', component: ChatPageComponent, canActivate:[authGuard] },
+    { path: 'chat/:requestId', component: ChatPageComponent , canActivate:[authGuard]}
+  ]}
 ];
---
+++ Frontend/src/app/components/dashboard/dashboard.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {Router, RouterLink, RouterOutlet} from "@angular/router";
+import {UserService} from '../../services/login/user.service';
+
+@Component({
+  selector: 'app-dashboard',
+    imports: [
+        RouterLink,
+        RouterOutlet
+    ],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css'
+})
+export class Dashboard {
+
+  constructor(private readonly router: Router,
+              protected readonly authService: UserService) {
+  }
+
+  logout(){
+    this.authService.logoutUser();
+    this.router.navigate(['/login'])
+  }
+
+}
